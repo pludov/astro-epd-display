c6232dfec1cff0c5f9a381df1e1eda0e45653e0e
Ludovic Pollet <ludovic.pollet@exfo.com>
2024-12-25 13:37:48 +0100

    partial update of the state

--- src/state.rs
+++ src/state.rs
@@ -6,7 +6,7 @@ use axum::{
 
 use once_cell::sync::Lazy;
 use serde_json::{Map, Number, Value};
-use std::sync::{Arc, Mutex};
+use std::{borrow::Borrow, sync::{Arc, Mutex}};
 
 use crate::trigger_draw;
 
@@ -26,6 +26,62 @@ enum Error {
     InvalidJsonValue,
 }
 
+// Value2 replaces value1 in case of conflict
+fn deep_merge(value1: &Value, value2: &Value) -> Value
+{
+    match value2 {
+        Value::Null => value2.clone(),
+        Value::Bool(_) => value2.clone(),
+        Value::Number(_) => value2.clone(),
+        Value::String(_) => value2.clone(),
+        Value::Array(arr2) => {
+            match value1 {
+                Value::Array(arr1) => {
+                    let mut outvec = Vec::new();
+                    for i in 0..arr2.len() {
+
+                        if i < arr1.len() {
+                            outvec.push(deep_merge(&arr1[i], &arr2[i]));
+                        } else {
+                            outvec.push(arr2[i].clone());
+                        }
+                    }
+                    Value::Array(outvec)
+                }
+                _ => Value::Array(arr2.clone()),
+            }
+        }
+        Value::Object(map2) => {
+            match value1 {
+                Value::Object(map1) => {
+                    let mut outmap = Map::new();
+                    // Create a set from map2 keys
+                    let map2keys: Vec<String> = map2.keys().cloned().collect();
+                    let map2keys: std::collections::HashSet<String> = map2keys.into_iter().collect();
+
+                    for (key, value) in map2 {
+                        if map1.contains_key(key) {
+                            outmap.insert(key.clone(), deep_merge(&map1[key], value));
+                        } else {
+                            outmap.insert(key.clone(), value.clone());
+                        }
+                    }
+                    // Also keep keys that are in map1 but not in map2
+                    for (key, value) in map1 {
+                        if !map2keys.contains(key) {
+                            outmap.insert(key.clone(), value.clone());
+                        }
+                    }
+                    Value::Object(outmap)
+                }
+                _ => Value::Object(map2.clone())
+            }
+        }
+    }
+
+}
+
+// This ensures that all numbers are f64, as implied by JSON RFC 7159
 fn cleanup(value: Value) -> Result<Value, Error> {
     match value {
         Value::Object(map) => {
@@ -36,7 +92,7 @@ fn cleanup(value: Value) -> Result<Value, Error> {
             Ok(Value::Object(outmap))
         }
         Value::Array(vec) => {
-            let mut outvec = vec.clone();
+            let mut outvec = Vec::new();
             for value in vec {
                 outvec.push(cleanup(value)?);
             }
@@ -76,9 +132,14 @@ async fn get_root() -> Json<Value> {
     Json((*get_state()).clone())
 }
 
+
 async fn post_root(payload: Json<Value>) -> Result<(), (StatusCode, String)> {
     let mut state = STATE.lock().unwrap();
-    state.root = Arc::new(cleanup(payload.0).or_else(handle_error)?);
+    // Do a deep merge of the state and the payload.
+    
+    let payload = cleanup(payload.0).or_else(handle_error)?;
+
+    state.root = Arc::new(deep_merge(state.root.borrow(), &payload));
 
     trigger_draw();
     Ok(())
@@ -86,4 +147,72 @@ async fn post_root(payload: Json<Value>) -> Result<(), (StatusCode, String)> {
 
 pub fn get_state() -> Arc<Value> {
     STATE.lock().unwrap().root.clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_deep_merge() {
+        let value1 = json!({
+            "a": 1,
+            "b": 2,
+            "c": {
+                "d": 3,
+                "e": 4,
+            },
+            "f": [1, 2, 3],
+        });
+
+        let value2 = json!({
+            "b": 3,
+            "c": {
+                "e": 5,
+                "f": 6,
+            },
+            "f": [4, 5, 6],
+        });
+
+        let expected = json!({
+            "a": 1,
+            "b": 3,
+            "c": {
+                "d": 3,
+                "e": 5,
+                "f": 6,
+            },
+            "f": [4, 5, 6],
+        });
+
+        let result = deep_merge(&value1, &value2);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_cleanup() {
+        let value = json!({
+            "a": 1,
+            "b": 2,
+            "c": {
+                "d": 3,
+                "e": 4,
+            },
+            "f": [1, 2, 3],
+        });
+
+        let expected = json!({
+            "a": 1.0,
+            "b": 2.0,
+            "c": {
+                "d": 3.0,
+                "e": 4.0,
+            },
+            "f": [1.0, 2.0, 3.0],
+        });
+
+        let result = cleanup(value).unwrap();
+        assert_eq!(result, expected);
+    }
 }
