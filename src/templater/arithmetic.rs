e1e5c4f8e3508e5defc5692d0569911146f352c9
Ludovic Pollet <ludovic.pollet@exfo.com>
2024-12-30 09:28:24 +0100

    sub (hiting a bug on tmpl itself)

--- src/templater/arithmetic.rs
+++ src/templater/arithmetic.rs
@@ -11,8 +11,17 @@ fn func_add(args: &[gtmpl::Value]) -> Result<gtmpl::Value, FuncError> {
     Ok(result.to_tmpl())
 }
 
+fn func_sub(args: &[gtmpl::Value]) -> Result<gtmpl::Value, FuncError> {
+    let mut result = args[0].to_float()?;
+    for a in &args[1..] {
+        result -= a.to_float()?;
+    }
+
+    Ok(result.to_tmpl())
+}
+
 pub fn funcs() -> Vec<(&'static str, Func)> {
-    vec![("add", func_add)]
+    vec![("add", func_add), ("sub", func_sub)]
 }
 
 #[cfg(test)]
@@ -28,6 +37,14 @@ mod test {
 
         tmpl.render(&Context::from(state)).unwrap()
     }
+    #[test]
+
+    fn test_round() {
+        let v: f64 = -4.4;
+        println!("v.fract() = {}", v.fract());
+        let n = gtmpl::Value::Number(gtmpl_value::Number::from(v));
+        assert_eq!(n.to_float().unwrap(), -4.4);
+    }
 
     // Test a simple rendering
     #[test]
@@ -41,5 +58,18 @@ mod test {
         );
         // Float
         assert_eq!(render(r#"{{ add 1.1 2.2 3.3 }}"#, Value::NoValue), "6.6");
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(render(r#"{{ sub 1 2 3 }}"#, Value::NoValue), "-4");
+        assert_eq!(render(r#"{{ sub 1.2 2.4 3 }}"#, Value::NoValue), "-4");
+        // 32 bits int overflow
+        assert_eq!(
+            render(r#"{{ sub -4294967296 -1 }}"#, Value::NoValue),
+            "-4294967295"
+        );
+        // Float
+        assert_eq!(render(r#"{{ sub 1.1 2.2 3.3 }}"#, Value::NoValue), "-4.4");
     }
 }
