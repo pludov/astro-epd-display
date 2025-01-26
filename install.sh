5912ed6e650098bb4a723f06f2dd635a6eba4e86
Ludovic P <pludow@gmail.com>
2025-01-26 22:46:32 +0100

    Fix install in docker env

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ install -o 0 -g 0 --mode=755 target/release/astro-epd-display /opt/astro-epd-dis
 install -o 0 -g 0 --mode=755 systemd/start-epd.sh /opt/astro-epd-display/
 install -o 0 -g 0 --mode=644 mobindi/template.yaml /opt/astro-epd-display/
 install -o 0 -g 0 --mode=644 systemd/astro-epd-display.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable astro-epd-display
+# This will fail if systemd is not running (e.g. in a chroot)
+systemctl daemon-reload || true
+systemctl enable astro-epd-display.service
+
